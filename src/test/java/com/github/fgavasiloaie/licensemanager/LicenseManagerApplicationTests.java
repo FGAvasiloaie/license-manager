a77e9bf9fca95e3a9268506b5895880c860fe279
floppi19 <florin.avasiloaie@gmail.com>
2025-07-01 07:54:07 +0300

    Initial commit

---
+++ src/test/java/com/github/fgavasiloaie/licensemanager/LicenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fgavasiloaie.licensemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
